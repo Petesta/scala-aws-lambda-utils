fc87c3af877ef4c562bd436e4c3e7be713168183
Pete Cruz <iPetesta@gmail.com>
2017-10-03 11:35:36 -0700

    Add Scalatest and refactor code.

--- src/main/scala/scala-aws-lambda-utils/Encoding.scala
+++ src/main/scala/scala-aws-lambda-utils/Encoding.scala
@@ -4,25 +4,30 @@ import io.circe.{ Decoder, Encoder, Error }
 import io.circe.parser._
 import io.circe.syntax._
 import java.io.{ InputStream, OutputStream }
+import scala.io.Source
 
 final case class Response[A](statusCode: Int, body: A)
 
+final case class Err(error: String)
+
 private[awslambda] trait Encoding {
   def in[A](is: InputStream)(implicit decoder: Decoder[A]): Either[Error, A] = {
-    val t = decode[A](scala.io.Source.fromInputStream(is).mkString)
+    val rawJson = Source.fromInputStream(is).mkString
+    val decodedJson = decode[A](rawJson)
     is.close()
-    t
+    decodedJson
   }
 
-  def out[A, B](response: Response[A], error: Response[B], os: OutputStream)(
+  def out[A](response: Response[A], os: OutputStream)(
     implicit encoder: Encoder[Response[A]],
-    errorEncoder: Encoder[Response[B]]
-  ): Either[Response[B], Response[A]] =
+    errDecoder: Encoder[Response[Err]]
+  ): Either[Response[Err], Response[A]] =
     try {
       os.write(response.asJson.noSpaces.getBytes("UTF-8"))
       Right(response)
     } catch {
       case e: Exception =>
+        val error = Response(500, Err(e.toString))
         os.write(error.asJson.noSpaces.getBytes("UTF-8"))
         Left(error)
     } finally {
